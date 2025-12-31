bf43567eb88133e9e67ec17b8a741f6e801d1b06
thereal-atom <oscarfal2006@gmail.com>
2025-12-31 13:41:56 +0000

    chore: components

---
+++ src/components/IconLink.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  interface Props {
+    href: string;
+    src: string;
+    alt: string;
+    text: string;
+  }
+
+  const { href, src, alt, text }: Props = $props();
+</script>
+
+<a
+  {href}
+  target="_blank"
+  class="group inline-flex items-center gap-1 align-middle text-white/70 hover:underline dark:text-white transition-all duration-200"
+>
+  <img
+    class="h-4 w-4 rounded-xs mb-0.5"
+    {src}
+    {alt}
+  />
+  <span>{text}</span>
+</a>
