289f5326de9fe31e248c39bb9fa1df5711afa9f3
thereal-atom <oscarfal2006@gmail.com>
2026-04-05 14:14:05 +0100

    fix: luma cal link & logo + ordering

--- src/data/eventSeries.ts
+++ src/data/eventSeries.ts
@@ -1,7 +1,7 @@
 import oscaramaLogo from "@/assets/logos/oscarama.png";
 import outventuringLogo from "@/assets/logos/out-buildering.png";
 import coloopLogo from "@/assets/logos/coloop-new.png";
-import demoNightsLogo from "@/assets/logos/demo-nights.png";
+import demoNightsLogo from "@/assets/logos/demo-nights.jpg";
 
 export const eventSeries = {
   ["oscaramas-stuff"]: {
@@ -12,15 +12,6 @@ export const eventSeries = {
       "a community for London's finest and most interested tech & AI builders and founders. hackathons, coworking, dinners and much more",
     lumaUrl: "https://luma.com/oscarama",
     lumaCalendarId: "cal-fTicl1ALRR1FGZt",
-  },
-  outventuring: {
-    name: "Out Venturing",
-    slug: "outventuring",
-    logo: outventuringLogo,
-    description:
-      "we don't like traditional networking events. come meet interesting people in interesting places instead. bouldering, ramen classes, bikepacking - you name it. we venture, out.",
-    lumaUrl: "https://lu.ma/outventuring",
-    lumaCalendarId: "cal-XR9yMbuVv84aRKj",
   },
   coloop: {
     name: "CoLoop Events",
@@ -36,8 +27,17 @@ export const eventSeries = {
     logo: demoNightsLogo,
     description:
       "a community of engineers, builders & super early founders. we run nights where people a curated set of builders show off what they have been working on in the AI space.",
-    lumaUrl: "https://lu.ma/london-ai",
+    lumaUrl: "https://lu.ma/londonmaxxing-ai",
     lumaCalendarId: "cal-BwhGlJaFM1pBN79",
+  },
+  outventuring: {
+    name: "Out Venturing",
+    slug: "outventuring",
+    logo: outventuringLogo,
+    description:
+      "we don't like traditional networking events. come meet interesting people in interesting places instead. bouldering, ramen classes, bikepacking - you name it. we venture, out.",
+    lumaUrl: "https://lu.ma/outventuring",
+    lumaCalendarId: "cal-XR9yMbuVv84aRKj",
   },
 } as const;
 
